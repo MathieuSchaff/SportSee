7a8004bbba5cf1f604c5e8225764d3b31dfcf706
Mathieu Schaff <schaff.mathieu93@gmail.com>
2022-08-03 17:44:59 +0200

    change main acceuil style - centering content

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,8 +1,8 @@
 // STYLE
-import './Title.scss'
+import "./Title.scss";
 // CONTEXT
-import {useContext} from 'react'
-import {UserContext} from '../../context/Context'
+import { useContext } from "react";
+import { UserContext } from "../../context/Context";
 
 /**
  *
@@ -13,8 +13,8 @@ const Title = () => {
   /**
    * get the data of the user from the context
    */
-  const context = useContext(UserContext)
-  const firstName = context?.response?.userInfos.firstName ?? undefined
+  const context = useContext(UserContext);
+  const firstName = context?.response?.userInfos.firstName ?? undefined;
   return (
     <div className="title__container">
       {context?.loading ? (
@@ -23,20 +23,21 @@ const Title = () => {
         <div>
           {context?.response && (
             <>
-              {' '}
+              {" "}
               <h1>
                 Bonjour,
                 <span className="red">{firstName}</span>
               </h1>
               <p className="objectifs">
-                Félicitation ! Vous avez explosé vos objectifs hier 👏{' '}
+                Félicitation ! Vous avez explosé vos objectifs hier 👏{" "}
               </p>
             </>
           )}
         </div>
       )}
+      <div style={{ visibility: "hidden" }}>DIV TO CENTER</div>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
